265f570457321f25c37c323697f29e20308aede8
Miguel Arciaga <miguelarciagaa@gmail.com>
2023-09-26 19:15:20 +0800

    shad cn test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from '@/components/ui/button'
 import './App.css'
 
 function App() {
@@ -18,9 +19,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+        <Button onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
