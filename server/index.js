f30ee1cca63bb3af18143ccb03fc33b6e22caf7d
Miguel Arciaga <miguelarciagaa@gmail.com>
2023-09-28 15:11:44 +0800

    be setup mongo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
+const connectDB = require('./db/connect');
+require("dotenv").config();
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("RFID")
 });
 
-app.listen(3001, () => {
-    console.log('Server is running at port 3001')
-});
+const startServer = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is listening to port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
