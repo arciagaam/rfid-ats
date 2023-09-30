8187c9ea0a962402a2b62d0c0aaba1b547b9c916
Ken Sagala <skendrick@teratomo.com>
2023-09-30 23:07:47 +0800

    Added logout reducer & endpoint, connected to fe

--- client/src/components/global/adminNav.tsx
+++ client/src/components/global/adminNav.tsx
@@ -1,31 +1,56 @@
 import React from 'react'
 import Navbar from './navbar'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '@/store/store'
+import { useLogoutMutation } from '@/slices/usersApiSlice'
+import { logout } from '@/slices/authSlice'
 import { Button } from '../ui/button'
 
 const AdminNav = () => {
-  return (
-    <>
-      <Navbar>
+    const { userInfo } = useSelector((state: RootState) => state.auth)
 
-        <div className="h-20 bg-white rounded-full min-h-5 aspect-square"></div>
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-        <div className="flex flex-col w-full h-full gap-5 px-5">
-          <Button asChild> 
-            <NavLink to='/admin'> Home </NavLink>
-          </Button>
+    const [logoutApiCall] = useLogoutMutation()
 
-          <Button asChild>
-            <NavLink to='register'> Register </NavLink>
-          </Button>
-        </div>
-      </Navbar>
+    const logoutHandler = async () => {
+        console.log('logout')
+        try {
+            await logoutApiCall().unwrap()
+            dispatch(logout())
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-      <div className="flex flex-col ml-[14rem] p-6">
-        <Outlet />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Navbar>
+                <div className='h-20 bg-white rounded-full min-h-5 aspect-square'></div>
+
+                <div className='flex flex-col w-full h-full gap-5 px-5'>
+                    <Button asChild>
+                        <NavLink to='/admin'> Home </NavLink>
+                    </Button>
+
+                    <Button asChild>
+                        <NavLink to='register'> Register </NavLink>
+                    </Button>
+
+                    <Button asChild onClick={logoutHandler}>
+                        <NavLink to='register'> Logout </NavLink>
+                    </Button>
+                </div>
+            </Navbar>
+
+            <div className='flex flex-col ml-[14rem] p-6'>
+                <Outlet />
+            </div>
+        </>
+    )
 }
 
 export default AdminNav
