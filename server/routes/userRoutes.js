ec8fa5595be340f05181f0a1f9311d1d1430fe93
Ken Sagala <skendrick@teratomo.com>
2023-09-30 16:28:58 +0800

    Added auth middleware to protect routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,13 +11,18 @@ import {
     updateUserByID,
     deleteUser,
 } from '../controller/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(registerUser).get(getUsers)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/logout', logoutUser)
-router.post('/login', authUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
-router.route('/:id').delete(deleteUser).get(getUserByID).put(updateUserByID)
+router.post('/auth', authUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router
+    .route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserByID)
+    .put(protect, admin, updateUserByID)
 
 export default router
