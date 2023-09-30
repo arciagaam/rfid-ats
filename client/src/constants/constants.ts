b85597b2f01a8a2feaf62dff80f61d22a8c216d2
Ken Sagala <skendrick@teratomo.com>
2023-09-30 22:44:56 +0800

    Config vite server, installed react-toastify

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3001' : ''
+// export const BASE_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3001' : ''
 
+export const BASE_URL = ''
 export const USERS_URL = '/api/users'
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,21 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 })
